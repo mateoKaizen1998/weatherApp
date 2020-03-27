d974014dbc37786cf8bb7828ca4f1cf345addb97
Mateo Sayas Pereira <mateo.sayas@fing.edu.uy>
2020-03-27 08:55:15 -0300

    first commit

---
+++ utils/weatherConditions.js
@@ -0,0 +1,51 @@
+export const weatherConditions = {
+  Rain: {
+    color: "#665E6D",
+    title: "LLuvioso",
+    subtitle: "Entra la ropa!",
+    icon: "weather-rainy"
+  },
+  Clear: {
+    color: "#FFD822",
+    title: "Muy soleado",
+    subtitle: "Usa lentes!",
+    icon: "weather-sunny"
+  },
+  Thunderstorm: {
+    color: "#3B4952",
+    title: "El cielo se tira pedos",
+    subtitle: "Andate pa' dentro!",
+    icon: "weather-lightning"
+  },
+  Clouds: {
+    color: "#6B7BA1",
+    title: "Nuboso",
+    subtitle: "Ponete protector igual",
+    icon: "weather-cloudy"
+  },
+
+  Snow: {
+    color: "#DBDBDB",
+    title: "Nieve",
+    subtitle: "Eso no pasa en Uruguay!",
+    icon: "weather-snowy"
+  },
+  Drizzle: {
+    color: "#076585",
+    title: "Llovizna",
+    subtitle: "Entra la ropa!",
+    icon: "weather-hail"
+  },
+  Haze: {
+    color: "#66A6FF",
+    title: "Haze",
+    subtitle: "Another name for Partly Raining",
+    icon: "weather-hail"
+  },
+  Mist: {
+    color: "#3CD3AD",
+    title: "Mist",
+    subtitle: "Microscopic water droplets",
+    icon: "weather-fog"
+  }
+};
